e034c328cb2a814eb3da7c0e19a3651bc25aa1df
Beatriz Tavares <66451513+BeatrizTavare-s@users.noreply.github.com>
2024-04-23 02:15:56 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseConnectionsResult = await database.query({
+    text: `select count(*)::int from pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
   response.status(200).json({
-    chave: "acima da média",
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(
+          databaseMaxConnectionsResult.rows[0].max_connections,
+        ),
+        opened_connections: databaseConnectionsResult.rows[0].count,
+        version: databaseVersionResult.rows[0].server_version,
+      },
+    },
   });
 }
 
